e781d709fc14ddfb49e2690ed6d965028a593f04
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-05-10 13:50:26 +0800

    memanggil program

--- REPOO1/REPOO1.cpp
+++ REPOO1/REPOO1.cpp
@@ -9,3 +9,17 @@ void mahasiswa::showNim() { //implementasi method deluar class
 	cout << "no induk =" << nim << endl;
 
 }
+int main()
+{
+	mahasiswa mhs{ 1 };  //object mhs
+	mhs.showNim();  //member acces operator
+
+	mahasiswa ref = mhs;  //pointer reference refMhs
+	ref.nim = 2;          //member acces operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; //pointer dereference pMhs
+	pMhs->nim = 3;          //acces operator
+	pMhs->showNim();
+	system("pause");
+}
