327d6dfe0e33799fa1b2f38631b445e1787009ff
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-05-10 12:55:03 +0800

    memasukan class siswa

--- REPO3/REPO3.cpp
+++ REPO3/REPO3.cpp
@@ -1,24 +1,3 @@
 #include <iostream>
 using namespace std;
 class siswa;
-class orang {
-private:
-	string nama;
-public:
-	void setNAma(string pNama);
-	friend class siswa;
-};
-class siswa {
-private:
-	int id;
-public:
-	void setId(int pId);
-	void displayALL(orang& a);
-};
-void siswa::displayALL(orang& a) {
-	cout << id << endl a.nama;
-
-}
-void siswa::setId(int pid) {
-
-}
